11b105e5e98e6a718a16ffdf4b73727e5235555e
shylujava <43169806+shylujava@users.noreply.github.com>
2018-10-04 12:06:21 +0530

    Add files via upload

---
+++ check for p+ or n-.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {int number=44;
+        if(number > 0)
+        {
+            System.out.println(number+" is a positive number");
+        }
+        else if(number < 0)
+        {
+            System.out.println(number+" is a negative number");
+        }
+        else
+        {
+            System.out.println(number+" is neither positive nor negative");
+        }
+}
+}
